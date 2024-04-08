27c0328187f9a78e202902c599ef0dda1dd0eba6
Mark Oudshoorn <markoudshoorn123@gmail.com>
2024-04-08 08:52:58 +0200

    Rework

--- Other/dbScraper.php
+++ Other/dbScraper.php
@@ -13,7 +13,6 @@
 
     session_start();
 
-
     foreach ($gps as $entry) {
         $stmt = $db->conn->prepare("SELECT COUNT(*) FROM gps WHERE title = :title");
         $stmt->bindParam(':title', $entry["name"]);
@@ -71,6 +70,64 @@
         ]);
     }
     }
+
+    // foreach ($gps as $entry) {
+    //     $stmt = $db->conn->prepare("SELECT COUNT(*) FROM gps WHERE title = :title");
+    //     $stmt->bindParam(':title', $entry["name"]);
+    //     $stmt->execute();
+    //     $count = $stmt->fetchColumn();
+    
+    //     if ($count > 0) {
+    //         echo "Skipped entry with name {$entry['name']}, already exists in the database.\n";
+    //     } else {
+    //     $gpsSQL->execute([
+    //         "_id" => $entry["id"],
+    //         "_title" => $entry["name"],
+    //         "_attribute_b2b" => $entry["attributes"]["B2B"] ?? null,
+    //         "_attribute_firmware" => $entry["attributes"]["firmware"] ?? null,
+    //         "_attribute_guarded" => $entry["attributes"]["guarded"] ?? null,
+    //         "_attribute_alarm" => $entry["attributes"]["alarm"] ?? null,
+    //         "_attribute_lastAlarm" => $entry["attributes"]["lastAlarm"] ?? null,
+    //         "_groupId" => $entry["groupId"] ?? null,
+    //         "_uniqueId" => $entry["uniqueId"] ?? null,
+    //         "_stat" => $entry["status"] ?? null,
+    //         "_lastUpdate" => $entry["lastUpdate"] ?? null,
+    //         "_positionId" => $entry["positionId"] ?? null,
+    //         "_geofenceIds" => $entry["geofenceIds"][0] ?? null,
+    //         "_phone" => $entry["phone"] ?? null,
+    //         "_model" => $entry["model"] ?? null,
+    //         "_contact" => $entry["contact"] ?? null,
+    //         "_category" => $entry["category"] ?? null,
+    //         "_isDisabled" => $entry["disabled"] ?? null,
+    //         "_positions_id" => $entry["positions"]["id"] ?? null,
+    //         "_positions_armed" => $entry["positions"]["attributes"]["armed"] ?? null,
+    //         "_positions_charge" => $entry["positions"]["attributes"]["charge"] ?? null,
+    //         "_positions_ignition" => $entry["positions"]["attributes"]["ignition"] ?? null,
+    //         "_positions_stat" => $entry["positions"]["attributes"]["status"] ?? null,
+    //         "_positions_distance" => $entry["positions"]["attributes"]["distance"] ?? null,
+    //         "_positions_totalDistance" => $entry["positions"]["attributes"]["totalDistance"] ?? null,
+    //         "_positions_motion" => $entry["positions"]["attributes"]["motion"] ?? null,
+    //         "_positions_batteryLevel" => $entry["positions"]["attributes"]["batteryLevel"] ?? null,
+    //         "_positions_hours" => $entry["positions"]["attributes"]["hours"] ?? null,
+    //         "_deviceId" => $entry["positions"]["deviceId"] ?? null,
+    //         "_thisType" => $entry["positions"]["type"] ?? null,
+    //         "_protocol" => $entry["positions"]["protocol"] ?? null,
+    //         "_serverTime" => $entry["positions"]["serverTime"] ?? null,
+    //         "_deviceTime" => $entry["positions"]["deviceTime"] ?? null,
+    //         "_fixTime" => $entry["positions"]["fixTime"] ?? null,
+    //         "_outdated" => $entry["positions"]["outdated"] ?? null,
+    //         "_valid" => $entry["positions"]["valid"] ?? null,
+    //         "_latitude" => $entry["positions"]["latitude"] ?? null,
+    //         "_longitude" => $entry["positions"]["longitude"] ?? null,
+    //         "_altitude" => $entry["positions"]["altitude"] ?? null,
+    //         "_speed" => $entry["positions"]["speed"] ?? null,
+    //         "_course" => $entry["positions"]["course"] ?? null,
+    //         "_thisAddress" => $entry["positions"]["address"] ?? null,
+    //         "_accuracy" => $entry["positions"]["accuracy"] ?? null,
+    //         "_network" => $entry["network"] ?? null
+    //     ]);
+    // }
+    // }
 
 
     // foreach ($vehicles as $vehicle) {
